15330aa85c5807680c714d0d563a01117e22cc4e
darwindev2002 <darwin0630@gmail.com>
2024-02-02 07:53:31 +0000

    Update backup setting for database

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -108,6 +108,14 @@ if 'PGHOST' in os.environ:
             conn_health_checks=True,
         )
     }
+else:
+    # Use SQLite @ BASE_DIR/db.sqlite3
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
